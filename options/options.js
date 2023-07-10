990ba1f81a6c83ca2862d2c5d08cd0f6cc603d3a
Crowdbotics <engineering@crowdbotics.com>
2023-07-10 20:38:39 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "muddy-wildflower-de-99612",
+  url: "https://muddy-wildflower-de-99612.botics.co",
+  api: "https://muddy-wildflower-de-99612.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
